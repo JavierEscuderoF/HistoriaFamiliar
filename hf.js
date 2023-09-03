5fae95a50ccdad58829815d8468eff0a84561fc5
Javier <javier.escudero90@gmail.com>
2023-09-03 20:00:39 +0200

    Principio del Doc1 y estilos

--- hf.js
+++ hf.js
@@ -1,9 +1,9 @@
 // Lista de todas las referencias en orden de fecha.
-let references = ["20","3"];
+let references = ["1"];
 
 // En la página Documentos, cabeceras con números consecutivos
 let init = 1;
 Array.from(document.getElementsByClassName("doc-header")).forEach(a => a.innerText = init++);
 
 // En las páginas con referencias, introduce el número correcto.
-Array.from(document.getElementsByClassName("ref")).forEach(a => a.innerHTML = "<sup><a href='../documentos.html#" + a.id + "'>[" + (references.indexOf(a.id) + 1) + "]</a></sup>" );
+Array.from(document.getElementsByClassName("ref")).forEach(a => a.innerHTML = "<sup><a class='ref-a' href='../documentos.html#" + a.getAttribute("data-doc") + "'>" + (references.indexOf(a.getAttribute("data-doc")) + 1) + "</a></sup>" );
